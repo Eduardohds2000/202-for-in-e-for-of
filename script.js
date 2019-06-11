bbfcb3cd8951805f5d4b02cec6ab60eebbef6ca6
eduardo <eduardodinuz23@gmail.com>
2019-06-11 17:02:53 -0300

    adicionar off e in

---
+++ script.js
@@ -0,0 +1,6 @@
+let signos= ["aries", "aquario", "cancer","peixes", "sagitario","escopiao","libra","gemeos" ];
+
+for(let valores in signos){
+
+    console.log("valor do indice "+ valores +" é: "+ signos [valores]);
+}   
